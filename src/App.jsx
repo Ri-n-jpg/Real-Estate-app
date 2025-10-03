e26c712d895a82f8906ce76ab39a66f84a945d3a
Ritika Sharma <ritikasharma29112003@gmail.com>
2025-10-03 21:56:00 +0530

    create pages and routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,21 @@
+import React from "react";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Signin from "./pages/Signin";
+import Profile from "./pages/Profile";
+
 export default function App() {
   return (
-    <div className="text-red-500 text-4xl font-bold">
-      Hello Tailwind!
-    </div>
-  )
+    <BrowserRouter>
+      {/* Optional navigation links */}
+      
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/signin" element={<Signin />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
