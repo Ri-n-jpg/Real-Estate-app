2d9c13ac2dbc9be07a258a4b12021727da4d6b8d
Ritika Sharma <ritikasharma29112003@gmail.com>
2025-10-04 01:04:04 +0530

    creates changes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,11 +2,11 @@ import { FaSearch } from 'react-icons/fa';
 import { Link} from 'react-router-dom';
 export default function Header() { 
  return (
-    <header className='bg-slate-200 shadow-md'>
+    <header className='bg-blue-200 shadow-md'>
       <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
         <Link to='/'>
           <h1 className='font-bold text-sm sm:text-xl flex flex-wrap'>
-            <span className='text-slate-500'>Sahand</span>
+            <span className='text-slate-500'>Ritika</span>
             <span className='text-slate-700'>Estate</span>
           </h1>
         </Link>
@@ -32,8 +32,9 @@ export default function Header() {
             <li className='hidden sm:inline text-slate-700 hover:underline'>
               About
             </li>
-          </Link>
-              <li className=' text-slate-700 hover:underline'> Sign in</li>
+          </Link> 
+          <Link to='/Signin'>
+              <li className=' text-slate-700 hover:underline'> Sign in</li></Link>
         
         </ul>
       </div>
